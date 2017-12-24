5f7252d1b641d640955a6ff1809661e7d0a0f6e9
John Susek <john@declaredintent.com>
2017-12-24 11:45:58 -0600

    Initial Commit

---
+++ Botcheck/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Botcheck
+//
+//  Created by John Susek on 12/24/17.
+//  Copyright © 2017 Declared Intent. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
