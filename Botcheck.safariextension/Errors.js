30ef44902651c90d33fa57f615e6f93bb8cc8264
John Susek <john@johnsolo.net>
2017-12-27 14:52:38 -0600

    no message

--- Botcheck.safariextension/Errors.js
+++ Botcheck.safariextension/Errors.js
@@ -1,16 +1,19 @@
 /* eslint-disable guard-for-in, no-restricted-syntax, no-empty */
 
 window.onerror = function(msg, url, line, col, error) {
-  let extra = !col ? '' : `\ncolumn: ${col}`;
-  extra += !error ? '' : `\nerror: ${error}`;
-  const message = `Error: ${msg}\nurl: ${url}\nline: ${line}${extra}`;
-
   const navi = {};
-  for (const navigatorProperty in navigator) {
-    if (typeof navigator[navigatorProperty] !== 'function') {
-      navi[navigatorProperty] = navigator[navigatorProperty];
+
+  try {
+    let extra = !col ? '' : `\ncolumn: ${col}`;
+    extra += !error ? '' : `\nerror: ${error}`;
+    const message = `Error: ${msg}\nurl: ${url}\nline: ${line}${extra}`;
+
+    for (const navigatorProperty in navigator) {
+      if (typeof navigator[navigatorProperty] !== 'function') {
+        navi[navigatorProperty] = navigator[navigatorProperty];
+      }
     }
-  }
+  } catch (err) {}
 
   // Log any unhandled errors - cheaper than newrelic :)
   try {
